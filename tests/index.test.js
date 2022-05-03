45661af2979f0d5fc64cc1c1a485d15e0677ebb9
Jorge Aguilar <jorge.agvel@gmail.com>
2022-05-02 19:25:54 -0500

    Solve test error adding a test default

---
+++ tests/index.test.js
@@ -0,0 +1,6 @@
+describe ("index.test.js", function () {        
+    test ("should return the index of the element", function () {
+        const sum = 5;
+        expect (sum).toBe (5);
+    });
+});
